27a36566205c64f5f26083c9878496817065cc44
Lucía Calisaya <calisaya.lucia@gmail.com>
2021-09-03 16:38:57 -0300

    Agrego proyecto para probar validaciones con FluentValidation

---
+++ FluentValidationLibrary/Data/FluentValidationLibraryContext.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidationLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FluentValidationLibrary.Data
+{
+    public class FluentValidationLibraryContext : DbContext
+    {
+        public FluentValidationLibraryContext (DbContextOptions<FluentValidationLibraryContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Product { get; set; }
+    }
+}
---
+++ FluentValidationLibrary/Migrations/20210903191114_InitialCreate.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FluentValidationLibrary.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Brand = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    IsExpiring = table.Column<bool>(type: "bit", nullable: false),
+                    Cost = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Product");
+        }
+    }
+}
