f391ed2f25731076b1ec8db0c3abe6197ac29d09
Lucía Calisaya <calisaya.lucia@gmail.com>
2021-09-20 15:38:57 -0300

    Se configura el objeto Log

---
+++ SerilogLibrary/Serilog.ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using Serilog.Sinks.SystemConsole.Themes;
+
+namespace Serilog.ConsoleApp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                             .MinimumLevel.Verbose()
+                             .WriteTo.Console(theme: AnsiConsoleTheme.Code)
+                             .CreateLogger();
+        }
+    }
+}
