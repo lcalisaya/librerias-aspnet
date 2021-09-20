b5cadf5672d8337fb5b21f2b1302d94ec62a7056
Lucía Calisaya <calisaya.lucia@gmail.com>
2021-09-20 16:12:21 -0300

    Se vuelca el log que genera Serilog en un archivo txt

--- SerilogLibrary/Serilog.File/Program.cs
+++ SerilogLibrary/Serilog.File/Program.cs
@@ -6,7 +6,31 @@ namespace Serilog.File
     {
         public static void Main(string[] args)
         {
-            
+            Log.Logger = new LoggerConfiguration()
+                             .MinimumLevel.Verbose()
+                             .WriteTo.File(@"C:\Users\lcalisaya\source\repos\librerias-aspnet\SerilogLibrary\Serilog.File\Logs\log.txt")
+                             .CreateLogger();
+
+            try
+            {
+                Log.Debug("Debugging my app with Serilog...");
+                Log.Information("Hello {Name}!", Environment.GetEnvironmentVariable("USERNAME"));
+                Log.Warning("It was expected to match another result here");
+
+                ThrowAnError();
+            }
+            catch (Exception e)
+            {
+                Log.Error(e.Message);
+                Log.Error(e, "An error occurred!");
+            }
+
+            Log.CloseAndFlush();
+        }
+
+        static void ThrowAnError()
+        {
+            throw new NullReferenceException();
         }
     }
 }
