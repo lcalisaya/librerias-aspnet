c7d422e42a785f69584903ea3b80720c095c2d9e
Lucía Calisaya <calisaya.lucia@gmail.com>
2021-09-21 17:05:54 -0300

    Se elige un proyecto ASP.NET Core y se instalan las dependencias de Hangfire

---
+++ HangfireLibrary/Hangfire.Intro/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Hangfire.Intro.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
