7862b878969e0d7e4b23e63a5f3f1905d2f54417
Lucía Calisaya <calisaya.lucia@gmail.com>
2021-09-23 18:02:17 -0300

    Se definen los pasos para que la aplicación Send envíe un mensaje a la aplicación Receive

--- RabbitMQLibrary/Receive/Receive.cs
+++ RabbitMQLibrary/Receive/Receive.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
 
 namespace Receive
 {
@@ -6,7 +9,35 @@ namespace Receive
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Creamos la conexión
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using(var connection = factory.CreateConnection())
+            {
+                //Creamos el canal
+                using(var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "hello",
+                                        durable: false,
+                                        exclusive: false,
+                                        autoDelete: false,
+                                        arguments: null);
+                    //La comunicación es asincrona entonces necesitamos un evento y un callback
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body.ToArray();
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine(" [x] Received {0}", message);
+                    };
+                    //Se informa al servidor que el mensaje ha sido recibido
+                    channel.BasicConsume(queue: "hello",
+                                        autoAck: true,
+                                        consumer: consumer);
+
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+                }
+            }
         }
     }
 }
--- RabbitMQLibrary/Send/Send.cs
+++ RabbitMQLibrary/Send/Send.cs
@@ -1,12 +1,41 @@
 ﻿using System;
-
+using RabbitMQ.Client;
+using System.Text;
 namespace Send
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Creamos una conexión
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using(var connection = factory.CreateConnection())
+            {
+                //Creamos un canal
+                using(var channel = connection.CreateModel())
+                {
+                    //Creamos una cola
+                    channel.QueueDeclare(queue: "hello",
+                                        durable: false,
+                                        exclusive: false,
+                                        autoDelete: false,
+                                        arguments: null);
+
+                    //Se crea el mensaje que se enviará y se lo transforma en un array de bytes
+                    string message = "Hello RabbitMQ!";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    //Se envía el mensaje
+                    channel.BasicPublish(exchange: "",
+                                        routingKey: "hello",
+                                        basicProperties: null,
+                                        body: body);
+                    Console.WriteLine(" [x] Sent {0}", message);
+                }
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
         }
     }
 }
