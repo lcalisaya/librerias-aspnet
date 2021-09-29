ac90931a05ce72731015f83a5628029461e90bdb
Lucía Calisaya <calisaya.lucia@gmail.com>
2021-09-29 15:57:41 -0300

    Agrego las clases que funcionarán como modelo para este ejemplo

---
+++ Json.NETLibrary/Json.Net.Sample/Model/Contact.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Json.Net.Sample.Model
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+        public Address Address { get; set; }
+        public List<Phone> Phones { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
---
+++ Json.NETLibrary/Json.Net.Sample/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Json.Net.Sample
+{
+    public class Program
+    {
+        private static string _path = @"C:\Json Sample\Contacts.json";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
